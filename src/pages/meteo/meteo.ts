99c319880e8976873ff378d7b45d415011c3c881
MostafaACHRAF <mostafaegma@gmail.com>
2017-11-26 13:45:54 +0000

    meteo api (openweathermap)

--- src/pages/meteo/meteo.ts
+++ src/pages/meteo/meteo.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, LoadingController} from 'ionic-angular';
+import {Http} from "@angular/http";
 
 /**
  * Generated class for the MeteoPage page.
@@ -15,11 +16,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MeteoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  meteo:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http, public loadingCtrl:LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MeteoPage');
+  }
+
+  onGetMeteo(dataForm) {
+    let loading = this.loadingCtrl.create({
+      content: "Loading..."
+    });
+
+    loading.present();
+
+    this.http.get("http://api.openweathermap.org/data/2.5/forecast?q=" + dataForm.ville + "&appid=ae99471c2f2ac13fc3618cf74219c5f2")
+      .map(resp => resp.json())
+      .subscribe(data => {
+        this.meteo = data;
+        loading.dismiss();
+      }, error => {
+        console.log(error);
+        console.log("error");
+        loading.dismiss();
+      });
   }
 
 }
